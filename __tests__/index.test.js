d8e79265fc3fe62be3b235a93be73c3e3f852abb
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2022-08-15 10:57:28 +0300

    fix lint

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,44 +1,46 @@
-import { getGreaterDivisor } from '../src/games/gcd';
-import { isPrime } from '../src/games/prime';
-import { isEven } from '../src/games/even';
-import { calculate } from '../src/games/calc';
+import { it, expect } from '@jest/globals';
 
-test('getGreaterDivisor(54, 96) should return 6', () => {
+import { getGreaterDivisor } from '../src/games/gcd.js';
+import { isPrime } from '../src/games/prime.js';
+import { isEven } from '../src/games/even.js';
+import { calculate } from '../src/games/calc.js';
+
+it('getGreaterDivisor(54, 96) should return 6', () => {
   expect(getGreaterDivisor(54, 96)).toBe(6);
 });
 
-test('getGreaterDivisor(12, 6) should return 6', () => {
+it('getGreaterDivisor(12, 6) should return 6', () => {
   expect(getGreaterDivisor(12, 6)).toBe(6);
 });
 
-test('isEven(28) should return true', () => {
+it('isEven(28) should return true', () => {
   expect(isEven(28)).toBe(true);
 });
 
-test('isPrime(5) should return true', () => {
+it('isPrime(5) should return true', () => {
   expect(isPrime(5)).toBe(true);
 });
 
-test('isPrime(4) should return false', () => {
+it('isPrime(4) should return false', () => {
   expect(isPrime(4)).toBe(false);
 });
 
-test('isPrime(1) should return false', () => {
+it('isPrime(1) should return false', () => {
   expect(isPrime(1)).toBe(false);
 });
 
-test('12 * 7 should return 84', () => {
+it('12 * 7 should return 84', () => {
   expect(calculate(12, 7, '*')).toBe(84);
 });
 
-test('12 - 7 should return 5', () => {
+it('12 - 7 should return 5', () => {
   expect(calculate(12, 7, '-')).toBe(5);
 });
 
-test('12 + 7 should return 19', () => {
+it('12 + 7 should return 19', () => {
   expect(calculate(12, 7, '+')).toBe(19);
 });
 
-test('12 / 7 should return empty string', () => {
+it('12 / 7 should return empty string', () => {
   expect(calculate(12, 7, '/')).toBe('');
 });
