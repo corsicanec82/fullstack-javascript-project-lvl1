db78aafe05c1043f1160ba6c5459404756f2dbd6
Stanislav Dzisiak <stanislav.dzisiak@gmail.com>
2024-05-27 11:35:01 +0300

    update texts for even game

--- src/games/even.js
+++ src/games/even.js
@@ -3,7 +3,7 @@ import { getRandom, cons } from '../utils.js';
 
 export const isEven = (number) => number % 2 === 0;
 
-const gameConditions = 'Answer "yes" if number even otherwise answer "no".';
+const gameConditions = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const getGameData = () => {
   const question = getRandom(10, 99);
